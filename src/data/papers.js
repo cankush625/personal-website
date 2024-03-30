3e19cda5e34e760401f6c772c7ff227608f51772
Ankush Chavan <cankush625@gmail.com>
2024-03-30 22:51:52 +0530

    feat(Paper Shelf): Add few more research papers and missing research paper links
    Signed-off-by: Ankush Chavan <cankush625@gmail.com>

--- src/data/papers.js
+++ src/data/papers.js
@@ -7,6 +7,17 @@ let papers = {
       image:
         "https://drive.google.com/thumbnail?id=1_noGJ3iJV1Pwl79oD06F4fdiAACt00Ja&sz=w900-h900",
       readDate: "2024-03-24",
+      link: "https://storage.googleapis.com/gweb-research2023-media/pubtools/pdf/10683a8987dbf0c6d4edcafb9b4f05cc9de5974a.pdf",
+    },
+    {
+      name: "Attention Is All You Need",
+      description:
+        "Talks about self-attention mechanism in machine translation tasks",
+      authors: "Ashish Vaswani, Noam Shazeer & others",
+      image:
+        "https://drive.google.com/thumbnail?id=1qu51PyAGB8KxcTT6f3YRbIKC1kTA0z7d&sz=w900-h900",
+      readDate: "2023-10-16",
+      link: "https://arxiv.org/pdf/1706.03762.pdf",
     },
   ],
   written: [
@@ -15,6 +26,7 @@ let papers = {
       description: "Image caption generator",
       authors: "Ankush Chavan, Kuldeepsingh Rajpurohit & others",
       image: "",
+      link: "https://link.springer.com/chapter/10.1007/978-981-16-7985-8_62",
     },
   ],
 };
