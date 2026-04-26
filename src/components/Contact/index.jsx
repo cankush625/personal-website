5d484d9e4e7767d1a1ba95b17dd35736a013d546
Ankush Chavan <cankush625@gmail.com>
2026-04-26 22:16:07 +0530

    feat: migrate from CRA to Vite and set up ShadCN
    Signed-off-by: Ankush Chavan <cankush625@gmail.com>

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -63,10 +63,10 @@ const Contact = () => {
 
     emailjs
       .sendForm(
-        process.env.REACT_APP_SERVICE_ID,
-        process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+        import.meta.env.VITE_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
         refForm.current,
-        process.env.REACT_APP_EMAILJS_PUBLIC_KEY
+        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
       )
       .then(
         () => {
